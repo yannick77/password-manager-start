617710b1faca2a56fc0a9cfdc33c88cd47c237ef
yannick77 <dongoyannick@gmail.com>
2026-08-08 23:07:31 +0200

    adding save function to a file

--- main.py
+++ main.py
@@ -2,15 +2,52 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-
+def save_password():
+    new_entry = f"Website: {website_entry.get()} | Email: {email_entry.get()} | Password: {password_entry.get()} \n"
+    with open("password.txt", "a") as file:
+        file.write(new_entry)
+    website_entry.delete(0, END)
+    website_entry.focus()
+    password_entry.delete(0, END)
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 canvas = Canvas(width=200, height=200)
 padlock_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=padlock_img)
-canvas.pack()
+canvas.grid(row=0, column=1)
+
+#Labels
+website_label = Label( text="Website:")
+website_label.grid(row=1, column=0)
+
+email_label = Label( text="Email/Username:")
+email_label.grid(row=2, column=0)
+
+password_label = Label( text="Password:")
+password_label.grid(row=3, column=0)
+
+#Entries
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "your_email@your_domain")
+password_entry = Entry(width=35)
+password_entry = Entry(width=35)
+
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+#Buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
+
+add_password_button = Button(text="Add", width=36, command=save_password)
+add_password_button.grid(row=4, column=1, columnspan=2)
 
 
 window.mainloop()
