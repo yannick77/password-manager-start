5a0ca49d95fda7946b1e379550d22055e3d4e915
yannick77 <dongoyannick@gmail.com>
2026-08-09 02:12:42 +0200

    added generate password functionality

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+password_letters = [random.choice(letters) for _ in range(nr_letters)]
+password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+
+password_list = password_letters + password_numbers + password_symbols
+random.shuffle(password_list)
+password = "".join(password_list)
+
+print(f"Your password is: {password}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
 
-    if website == "" or email == "" or password == "":
+    if len(website)==0 or len(email)==0 or len(password) == 0:
         messagebox.showerror("Error", "Please enter all required information")
     else:
         new_entry = f"Website: {website} | Email: {email} | Password: {password} \n"
